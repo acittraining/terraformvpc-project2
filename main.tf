ccb956c20815edd3014dc0aaf22a7107f7a88b24
JerryBame <106651265+acittraining@users.noreply.github.com>
2023-08-25 20:44:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -214,6 +214,20 @@ resource "aws_security_group" "demo-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
