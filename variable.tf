9edaba6dbe2dc8b03986115ce299a420e95fa35f
JerryBame <106651265+acittraining@users.noreply.github.com>
2023-08-25 17:08:07 +0200

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {}
+variable "vpccidr" {}
+variable "pubsub1cidr" {}
+variable "pubsub2cidr" {}
+variable "pubsub3cidr" {}
+variable "prisub1cidr" {}
+variable "prisub2cidr" {}
+variable "prisub3cidr" {}
